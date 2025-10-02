fea474643ab7da82b16b20cd999135e06b15b934
JordanEdenthizz228 <blackfamas6@gmail.com>
2025-10-02 21:45:24 +0300

    Добавление плагинаций в бэкенд

---
+++ hotels.py
@@ -0,0 +1,83 @@
+from fastapi import HTTPException, APIRouter, Query
+
+from schemas.shema import Hotels, HotelsPATCH, PaginatedResponse
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
+]
+
+
+@router.get("/")
+def print_hotels():
+    return hotels
+
+
+@router.get("/paginated", response_model=PaginatedResponse)
+async def get_page_hotel(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(3, ge=1)
+):
+    total = len(hotels)
+    start = (page - 1) * per_page
+    end = start + per_page
+    data = hotels[start:end]
+    return PaginatedResponse(
+        total=total,
+        page=page,
+        per_page=per_page,
+        data=data
+    )
+
+@router.put("/{hotel_id}")
+async def update_hotel(
+    hotel_id: int, 
+    hotel_data: Hotels
+):  
+    if not hotel_data.title.strip():
+        raise HTTPException(400, "Title cannot be empty")
+    if not hotel_data.name.strip():
+        raise HTTPException(400, "Name cannot be empty")
+    
+    hotel = next((h for h in hotels if h["id"] == hotel_id), None)
+    if not hotel:
+        raise HTTPException(400, "Hotel is not found")
+    
+    hotel["title"] = hotel_data.title.strip()
+    hotel["name"] = hotel_data.name.strip()
+    
+    return {"message": "Hotel updated", "hotel": hotel}
+
+
+@router.patch("/{hotel_id}")
+async def partial_update_hotel(
+    hotel_id: int,
+    hotel_data: HotelsPATCH
+):
+    if hotel_data.title is None and hotel_data.name is None:
+        raise HTTPException(400, "Title and name cannot be None")
+    
+    if hotel_data.title is not None and not hotel_data.title.strip():
+        raise HTTPException(400, "Title cannot be empty")
+    if hotel_data.name is not None and not hotel_data.title.strip():
+        raise HTTPException(400, "Name cannot be empty")
+
+    hotel = next((h for h in hotels if h["id"] == hotel_id), None)
+    if not hotel:
+        raise HTTPException(400, "Hotel isn`t found")
+    
+    if hotel_data.title is not None:
+        hotel["title"] = hotel_data.title.strip()
+    if hotel_data.name is not None:
+        hotel["name"] = hotel_data.name.strip()
+        
+    return {"message": "Hotel partially update", "hotel": hotel}
+    
+    
---
+++ schemas/shema.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class Hotels(BaseModel):
+    id: int
+    name: str
+    title: str
+
+class HotelsPATCH(BaseModel):
+    title:  str | None = Field(None)
+    name: str | None = Field(None)
+
+
+class PaginatedResponse(BaseModel):
+    total: int # сколько отелей находятся в списке
+    page: int 
+    per_page: int 
+    data: List[Hotels]
